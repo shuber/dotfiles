76becf1a20aae3e69664d5ff6ea231b972307297
Sean Huber <sean@lendinghome.com>
2015-11-25 22:01:22 -0800

    Remove jobs option from bi alias

--- shell/ruby.sh
+++ shell/ruby.sh
@@ -8,7 +8,7 @@ fi
 
 alias b="bundle"
 alias be="bundle exec"
-alias bi="bundle install --path vendor/bundle --jobs 7"
+alias bi="bundle install --path vendor/bundle"
 alias bs="bundle exec rails server"
 alias bu="bundle update"
 alias rdm="bundle exec rake db:migrate db:test:clone"
