079af6543c14df22f20c7999872863a1fe2bb804
Sean Huber <sean@lendinghome.com>
2017-08-23 22:17:04 -0700

    Stop using reattach-to-user-namespace

--- shell/vim.sh
+++ shell/vim.sh
@@ -4,7 +4,3 @@ alias edit-vim-bundles="$EDITOR $HOME/.vimrc.bundles"
 alias edit-vim-conf="$EDITOR $HOME/.vimrc"
 alias edit-vim-local="$EDITOR $HOME/.vimrc.local"
 alias reload-vim-bundles="$EDITOR vim +BundleInstall +qall"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  alias vim="reattach-to-user-namespace vim"
-fi
